1be7e7c8c0075016d0a2b48a97b2ff2d607efaaa
“jiayinchu” <yinchu_jia@kingdee.com>
2024-05-25 20:03:48 +0800

    fix: modified app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MONGO_URI } from './utils';
-import { UploadModule } from './upload/upload.module';
+// import { UploadModule } from './upload/upload.module';
 @Module({
     imports: [
         // MongooseModule.forRoot(MONGO_URI),
-        UploadModule,
+        // UploadModule,
     ],
     controllers: [AppController],
     providers: [AppService],
