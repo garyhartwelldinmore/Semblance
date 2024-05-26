46d5aa3a7a1200511d7db8509cb277def476e877
“jiayinchu” <yinchu_jia@kingdee.com>
2024-05-26 13:47:48 +0800

    fix: change welcome message

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
     constructor(private readonly appService: AppService) { }
 
     @Get()
-    @Render('index.hbs')
+    @Render('index.html')
     getIndex() {
         return { message: 'Hello world!' };
     }
